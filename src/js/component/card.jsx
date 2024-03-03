76d16b97c6ef41d8e4bf49b4715636de9427309d
Diana024 <154326533+Diana024@users.noreply.github.com>
2024-03-03 17:04:25 +0000

    Website Diana React 2024

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -9,11 +9,13 @@ const Card = (props) => {
 	return (
 	<div className="col-12 col-md-6 col-lg-3 mb-4">
         <div className="card">
-        <img src={props.image} className="card-img-top" alt="..."/>
-        <div className="card-body text-center">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.description}Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, sequi ipsum minus ullam nam aut, modi eaque reprehenderit nesciunt cum ad voluptate sapiente neque, consequuntur omnis. </p>
-        <a href={props.buttonURL} className="btn btn-primary btn-sm">Find Out More!</a>
+          <img src={props.image} className="card-img-top" alt="..."/>
+          <div className="card-body text-center">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, sequi ipsum minus ullam nam aut, modi eaque reprehenderit nesciunt cum ad voluptate sapiente neque, consequuntur omnis. </p>
+        </div>
+        <div class="card-footer text-center">
+          <a href={props.buttonURL} className="btn btn-primary btn-sm">Find Out More!</a>
         </div>
     </div>
 </div>
